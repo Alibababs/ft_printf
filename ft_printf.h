bbc193cce022cb3dffd0394ebffcf1a2873c1014
Alibababs <alibaba@Alix>
2024-05-30 19:33:21 +0200

    tjrs pas

--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,14 @@
 /*   By: alibaba <alibaba@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 14:19:00 by pbailly           #+#    #+#             */
-/*   Updated: 2024/05/30 19:20:07 by alibaba          ###   ########.fr       */
+/*   Updated: 2024/05/30 19:29:16 by alibaba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include "libft/libft.h"
 # include <stdarg.h>
 # include <unistd.h>
 
--- ft_printfnbr.c
+++ ft_printfnbr.c
@@ -6,41 +6,20 @@
 /*   By: alibaba <alibaba@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 16:58:41 by pbailly           #+#    #+#             */
-/*   Updated: 2024/05/30 19:24:26 by alibaba          ###   ########.fr       */
+/*   Updated: 2024/05/30 19:31:24 by alibaba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int     count_size(long int n)
-{
-        size_t  i;
-
-        i = 0;
-        if (n == 0)
-                return (1);
-        if (n < 0)
-                n *= -1;
-        while (n)
-        {
-                n /= 10;
-                i++;
-        }
-        return (i);
-}
-
 int	ft_printfnbr(int n)
 {
-	int	len;
+	int		len;
+	char	*num;
 
 	len = 0;
-	if (n == -2147483648)
-	{
-		len = ft_putstr("-2147483648");
-		return (len);
-	}
-
-
-
+	num = ft_itoa(n);
+	len = ft_printfstr(num);
+	free(num);
 	return (len);
 }
